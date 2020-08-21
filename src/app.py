b64f8dc7d4daae7977a68e5fd6983296e57f2ac7
Janne Mattila <janne.mattila@hotmail.fi>
2020-08-21 13:47:31 +0300

    Fix missing parentheses problem

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-print "Text coming from container"
+print ("Text coming from container")
 
-print "##vso[task.setvariable variable=Custom.WebAppUri;]https://github.com/JanneMattila/330-SimplePythonExample"
-print "##vso[task.setvariable variable=Custom.Username;]JanneMattila"
+print ("##vso[task.setvariable variable=Custom.WebAppUri;]https://github.com/JanneMattila/330-SimplePythonExample")
+print ("##vso[task.setvariable variable=Custom.Username;]JanneMattila")
